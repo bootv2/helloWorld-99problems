45d85c9151f2d61be43cc4838cec942e2d871e53
Tim Boode <tim@compuboot.in>
2014-09-04 10:05:42 +0200

    commented

--- HelloWorldApplication/src/helloworldapplication/HelloWorldApplication.java
+++ HelloWorldApplication/src/helloworldapplication/HelloWorldApplication.java
@@ -10,15 +10,17 @@ package helloworldapplication;
  *
  * @author tim
  */
-public class HelloWorldApplication {
+public class HelloWorldApplication 
+{
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World!");//print hello world
         
-        for(int i = 1; i < 100; i++)
+        for(int i = 1; i < 100; i++)//count up to 100, and print "Got + count + problems but a bitch aint one"
             System.out.println("Got " + i + " problems, but a bitch ain't one");
     }
     
